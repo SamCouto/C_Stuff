ff53101df88bb0accfcb4a27795b58408ffa8f21
Samuel Couto Silva <samuelcouto2003@outlook.com>
2023-08-25 22:03:23 -0300

    My first program in C
    Hello word!

---
+++ beginning.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello world, this is my first program lines in C.\n");
+  printf("Let's add another line to make our program bigger!\n");
+  printf("Then lets finish it, because this reading is taking too lonng.\n");
+  return 0;
+}
