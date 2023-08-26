4bf326544306b3b182f85feded0bbd80bdff33e1
Samuel Couto Silva <samuelcouto2003@outlook.com>
2023-08-26 15:02:39 -0300

    Update beginning.c

--- beginning.c
+++ beginning.c
@@ -4,6 +4,6 @@ int main()
 {
   printf("Hello world, this is my first program lines in C.\n");
   printf("Let's add another line to make our program bigger!\n");
-  printf("Then lets finish it, because this reading is taking too lonng.\n");
+  printf("Then lets finish it, because this reading is taking too long.\n");
   return 0;
 }
